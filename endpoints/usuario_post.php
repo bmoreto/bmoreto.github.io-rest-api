ddf2259025c37dc92dd889800f5e4d060b08103c
bmoreto <beatriz_moreto@hotmail.com>
2025-06-17 06:55:48 -0300

    usuario GET e PUT

--- endpoints/usuario_post.php
+++ endpoints/usuario_post.php
@@ -1,7 +1,6 @@
 <?php
 
-function api_usuario_post($request)
-{
+function api_usuario_post($request) {
   $email = sanitize_email($request['email']);
   $senha = $request['senha'];
   $nome = sanitize_text_field($request['nome']);
@@ -15,16 +14,15 @@ function api_usuario_post($request)
   $user_exists = username_exists($email);
   $email_exists = email_exists($email);
 
-  if (!$user_exists && !$email_exists && $email && $senha) {
+  if(!$user_exists && !$email_exists && $email && $senha) {
     $user_id = wp_create_user($email, $senha, $email);
 
     $response = array(
-      'ID' =>  $user_id,
+      'ID' => $user_id,
       'display_name' => $nome,
       'first_name' => $nome,
       'role' => 'subscriber',
     );
-
     wp_update_user($response);
 
     update_user_meta($user_id, 'cep', $cep);
@@ -36,12 +34,10 @@ function api_usuario_post($request)
   } else {
     $response = new WP_Error('email', 'Email já cadastrado.', array('status' => 403));
   }
-
   return rest_ensure_response($response);
 }
 
-function registrar_api_usuario_post()
-{
+function registrar_api_usuario_post() {
   register_rest_route('api', '/usuario', array(
     array(
       'methods' => WP_REST_Server::CREATABLE,
@@ -51,3 +47,6 @@ function registrar_api_usuario_post()
 }
 
 add_action('rest_api_init', 'registrar_api_usuario_post');
+
+
+?>
